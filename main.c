5b94cced2c2eb8bd63f9716e55f90b056fc141db
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-08-06 15:55:54 -0400

    Update main.cmugriento

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("School\n");
-  }
-  return 0;
+    printf("Hello,School");
+  
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
